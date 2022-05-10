bce23275700b7e731f57b902fedad88fefecf841
Your Namegit config --global user.name Your <you@example.com>
2022-05-09 21:05:52 -0400

    Adding CSS

--- index.js
+++ index.js
@@ -1,18 +1,25 @@
-require("dotenv").config();
-const express = require("express");
-const app = express();
+// Modules and Globals
+require('dotenv').config()
+const express = require('express')
+const app = express()
 
-app.set("view engine", "jsx");
-app.engine("jsx", require("express-react-views").createEngine());
+// Express Settings
+app.set('views', __dirname + '/views')
+app.set('view engine', 'jsx')
+app.engine('jsx', require('express-react-views').createEngine())
+app.use(express.static('public'))
 
-app.use("/places", require("./controllers/places"));
+// Controllers & Routes
+app.use('/places', require('./controllers/places'))
 
-app.get("/", (req, res) => {
-  res.render("home");
-});
+app.get('/', (req, res) => {
+    res.render('home')
+})
 
-app.get("*", (req, res) => {
-  res.status(404).render("error404");
-});
+app.get('*', (req, res) => {
+    res.render('error404')
+})
 
-app.listen(process.env.PORT);
+// Listen for Connections
+app.listen(process.env.PORT)
+
