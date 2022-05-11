1dc8d74628de01065ddfa844ac758b99acbdf868
Your Namegit config --global user.name Your <you@example.com>
2022-05-11 18:51:40 -0400

    Navbar

--- views/default.jsx
+++ views/default.jsx
@@ -5,10 +5,28 @@ function Def(html) {
     <html>
       <head>
         <title>Title</title>
-        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" />
+        <link
+          rel="stylesheet"
+          href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css"
+        />
         <link rel="stylesheet" href="/css/style.css" />
       </head>
-      <body>{html.children}</body>
+      <body>
+        <nav>
+          <ul>
+            <li>
+              <a href="/">Home</a>
+            </li>
+            <li>
+              <a href="/places">Places</a>
+            </li>
+            <li>
+              <a href="/places/new">Add Place</a>
+            </li>
+          </ul>
+        </nav>
+        {html.children}
+      </body>
     </html>
   );
 }
