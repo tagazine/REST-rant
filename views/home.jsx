889375f8df05a6b25bdfaf23a76e1185af9dc388
Your Namegit config --global user.name Your <you@example.com>
2022-05-11 14:30:09 -0400

    Bootstrap

--- views/home.jsx
+++ views/home.jsx
@@ -5,7 +5,7 @@ function home() {
   return (
     <Def>
       <main>
-        <h1>HOME</h1>
+        <h1>REST-Rant</h1>
         <div>
           <img src="/images/market.jpg" alt="Market" height='300px' width='500px'/>
           <div>Photo by <a href="https://unsplash.com/@thomasble?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Thomas Le</a> on <a href="unsplash.com">Unsplash</a></div>
--- views/places/index.jsx
+++ views/places/index.jsx
@@ -6,25 +6,23 @@ function places(data) {
     return (
       <div className="col-sm-6">
         <h2>{place.name}</h2>
-        <p className="text-center">
-          {place.cuisines}
-        </p>
+        <p className="text-center">{place.cuisines}</p>
         <img src={place.pic} alt={place.name} />
         <p className="text-center">
           Located in {place.city}, {place.state}
         </p>
       </div>
-    )
-  })
-  
+    );
+  });
+
   return (
-      <Def>
-          <main>
-              <h1>Rant or Rave Here</h1>
-              {placesFormatted}
-          </main>
-      </Def>
-  )
+    <Def>
+      <main>
+        <h1>Rant or Rave Here</h1>
+        <div className="row">{placesFormatted}</div>
+      </main>
+    </Def>
+  );
 }
 
 module.exports = places;
