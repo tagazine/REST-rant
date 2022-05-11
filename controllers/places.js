13491093d15ad5c03d2dca13f79dd854660d0a93
Your Namegit config --global user.name Your <you@example.com>
2022-05-11 15:47:55 -0400

    places can be added

--- controllers/places.js
+++ controllers/places.js
@@ -1,34 +1,27 @@
 const router = require("express").Router();
+const places = require("../models/places.js");
 
 router.get("/new", (req, res) => {
   res.render("places/new");
 });
 
 router.post("/", (req, res) => {
-  res.render("places/index");
+  console.log(req.body);
+  if (!req.body.pic) {
+    req.body.pic = "http://placekitten.com/400/400";
+  }
+  if (!req.body.city) {
+    req.body.city = "Anytown";
+  }
+  if (!req.body.state) {
+    req.body.state = "USA";
+  }
+  places.push(req.body);
+  res.redirect("/places");
 });
 
 router.get("/", (req, res) => {
-  let places = [
-    {
-      name: "H-Thai-ML",
-      city: "Seattle",
-      state: "WA",
-      cuisines: "Thai, Pan-Asian",
-      pic: "/images/thai.jpg",
-    },
-    {
-      name: "Coding Cat Cafe",
-      city: "Phoenix",
-      state: "AZ",
-      cuisines: "Coffee, Bakery",
-      pic: "/images/coffee.jpg",
-    },
-  ];
-
   res.render("places/index", { places });
 });
-
-
 
 module.exports = router;
