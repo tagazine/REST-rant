563a4c0f8c2d98632cb232d53db2e30eca6787aa
Your Namegit config --global user.name Your <you@example.com>
2022-05-11 15:35:45 -0400

    Jazzed up form (but still issues with post/place

--- controllers/places.js
+++ controllers/places.js
@@ -1,5 +1,13 @@
 const router = require("express").Router();
 
+router.get("/new", (req, res) => {
+  res.render("places/new");
+});
+
+router.post("/", (req, res) => {
+  res.render("places/index");
+});
+
 router.get("/", (req, res) => {
   let places = [
     {
@@ -21,8 +29,6 @@ router.get("/", (req, res) => {
   res.render("places/index", { places });
 });
 
-router.get('/new', (req, res) => {
-  res.render('places/new')
-})
+
 
 module.exports = router;
