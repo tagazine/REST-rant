70d69dd9ba2f405228c9a931e6c635c4e03451c6
Your Namegit config --global user.name Your <you@example.com>
2022-05-19 16:32:12 -0400

    Seed Revisions

--- views/places/show.jsx
+++ views/places/show.jsx
@@ -6,28 +6,34 @@ function show(data) {
     <html>
       <Def>
         <main>
-          <h1>{data.place.name}</h1>
-          <img src={data.place.pic} alt={data.place.name} />
-          <h2>Rating: *****</h2>
-          <h2>Description: {data.place.cuisines}</h2>
-          <h2>
-            {data.place.city}, {data.place.state}
-          </h2>
-          <br />
-          <br />
+          <div className="row">
+            <div className="col-sm-6">
+              <img src={data.place.pic} alt={data.place.name} />
+              <h2>
+                {data.place.city}, {data.place.state}
+              </h2>
+            </div>
+            <div className="col-sm-6">
+              <h1>{data.place.name}</h1>
+              <h2>Rating</h2>
+              <h2>*****</h2>
+              <h2>Description: </h2>
+              <h3>{data.place.showEstablished()}</h3>
+              <h4>Serving {data.place.cuisines}</h4>
+              <br />
+              <a href={`/places/${data.id}/edit`} className="btn btn-warning">
+                Edit
+              </a>
+              <form method="POST" action={`/places/${data.id}?_method=DELETE`}>
+                <button type="submit" className="btn btn-danger">
+                  Delete
+                </button>
+              </form>
+            </div>
+          </div>
         </main>
         <h2>Comments</h2>
-        <br />
-        <br />
-        <a href={`/places/${data.id}/edit`} className="btn btn-warning">
-          Edit
-        </a>
-        <br />
-        <form method="POST" action={`/places/${data.id}?_method=DELETE`}>
-          <button type="submit" className="btn btn-danger">
-            Delete
-          </button>
-        </form>
+        <h3>No comments yet!</h3>
       </Def>
     </html>
   );
